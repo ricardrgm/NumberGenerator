1e74c1da97ff2a01488fd88ca2d91b1cc4641359
ricard <ricard.garcia@grupoica.com>
2017-07-06 14:29:20 +0200

    first commit

---
+++ Numbergenerator/src/test/java/com/example/Numbergenerator/AppTest.java
@@ -0,0 +1,22 @@
+package com.example.Numbergenerator;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+
+    
+
+
+
+public class AppTest {
+
+    @Test
+    public void testA() {
+       assertTrue(true);
+
+    }
+}
+
